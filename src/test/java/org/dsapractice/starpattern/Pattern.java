5392e73b591a66ffe98d7ab2524745ed3340e1a8
Mahalakshmi <lekshmi0101@gmail.com>
2024-08-15 19:22:54 +0530

    My practice

--- src/test/java/org/dsapractice/starpattern/Pattern.java
+++ src/test/java/org/dsapractice/starpattern/Pattern.java
@@ -51,5 +51,28 @@ public class Pattern {
             }
             System.out.println();
         }
+
+//   *
+//  **
+// ***
+//****
+
+        System.out.println("Solution for Q4");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                if(j>=4-(i+1))
+                {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
-}
+
+    }
+
+
