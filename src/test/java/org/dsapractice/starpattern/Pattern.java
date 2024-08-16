6c159d586f0a74a1affee5ff48e1d9ac06f9f43c
Mahalakshmi <lekshmi0101@gmail.com>
2024-08-16 11:59:56 +0530

    My practice

--- src/test/java/org/dsapractice/starpattern/Pattern.java
+++ src/test/java/org/dsapractice/starpattern/Pattern.java
@@ -72,7 +72,6 @@ public class Pattern {
             System.out.println();
         }
     }
-
     }
 
 
---
+++ src/test/java/org/dsapractice/starpattern/Rhombus.java
@@ -0,0 +1,21 @@
+package org.dsapractice.starpattern;
+
+public class Rhombus {
+
+    public static void main(String[] args) {
+
+        int rows = 5;
+        int cols = 5;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            System.out.print(sb.append(" "));
+        }
+    }
+}
+
+
