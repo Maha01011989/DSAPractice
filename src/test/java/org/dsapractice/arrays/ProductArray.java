a2c73ac39a4a5d0695c09567e01c3f094e3a3773
Mahalakshmi <lekshmi0101@gmail.com>
2024-08-19 23:39:43 +0530

    My practice

--- src/test/java/org/dsapractice/arrays/ProductArray.java
+++ src/test/java/org/dsapractice/arrays/ProductArray.java
@@ -8,23 +8,18 @@ public class ProductArray {
     public static void main(String[] args) {
 
 
-        int[] num = {1, 2, 3,4};
+        int[] num = {1, 2, 3, 4};
 
         int[] ans = new int[num.length];
 
         for (int i = 0; i < num.length; i++) {
-            int j = 0;
-            int p = 1;
 
-            while (j < num.length) {
+            int p = 1;
+            for (int j = 0; j < num.length; j++) {
                 if (j == i) {
-                    j++;
+                    continue;
                 }
                 p = p * num[j];
-                j++;
-                if (i == num.length - 1 && j == num.length - 1) {
-                    break;
-                }
             }
 
             ans[i] = p;
