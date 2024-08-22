6f27d48bf0a225002450f86eef8152b188e6fd0e
Mahalakshmi <lekshmi0101@gmail.com>
2024-08-22 16:37:30 +0530

    My practice

--- src/test/java/org/dsapractice/starpattern/NumberTriangle.java
+++ src/test/java/org/dsapractice/starpattern/NumberTriangle.java
@@ -23,7 +23,6 @@ public class NumberTriangle {
                 }
             }
             System.out.println();
-
         }
     }
 }
---
+++ src/test/java/org/dsapractice/starpattern/ZeroOneTriangle.java
@@ -0,0 +1,17 @@
+package org.dsapractice.starpattern;
+
+public class ZeroOneTriangle {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                if (i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
