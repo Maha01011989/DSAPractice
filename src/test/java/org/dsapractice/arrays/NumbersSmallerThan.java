5959a38adfb582fbea9bf4a4cf6f6fd6d3364957
Mahalakshmi <lekshmi0101@gmail.com>
2024-08-29 18:56:58 +0530

    My practice

---
+++ src/test/java/org/dsapractice/arrays/NumbersSmallerThan.java
@@ -0,0 +1,34 @@
+package org.dsapractice.arrays;
+
+import java.util.Arrays;
+
+public class NumbersSmallerThan {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5,4,7,2,1};
+
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(arr)));
+
+
+    }
+
+
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int[] arr = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (j == i)
+                    continue;
+                if (nums[i] > nums[j])
+                    count++;
+            }
+            arr[i] = count;
+        }
+        return arr;
+
+    }
+}
