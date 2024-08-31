85c4028a0d5c82dbf837ac4b28b7442417e845e7
lakshmi <lekshmi0101@gmail.com>
2024-08-31 17:42:38 +0530

    My practice

---
+++ src/test/java/org/dsapractice/arrays/MissingNumber.java
@@ -0,0 +1,41 @@
+package org.dsapractice.arrays;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 2};
+
+        System.out.println(missingNumber(nums));
+
+    }
+
+    static int missingNumber(int[] nums) {
+
+        {
+
+            int i = 0;
+            while (i < nums.length) {
+                if (nums[i]<nums.length && nums[i] != i) {
+                    int temp = nums[nums[i]];
+                    nums[nums[i]] = nums[i];
+                    nums[i] = temp;
+                } else {
+                    i++;
+                }
+            }
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] != j) {
+                    return j;
+                }
+            }
+            return nums.length;
+        }
+    }
+}
+
+
+
+
--- src/test/java/org/dsapractice/sorting/BubbleSort.java
+++ src/test/java/org/dsapractice/sorting/BubbleSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 4, 3, 5};
+        int[] arr = {8, 2, 4, 3, 5};
 
         for (int i = 0; i < arr.length; i++) {
             boolean sorted = true;
