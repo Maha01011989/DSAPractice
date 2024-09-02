22d01e1d6e2e1f7b0115c07db0133b230e4671e8
Mahalakshmi <lekshmi0101@gmail.com>
2024-09-02 17:46:00 +0530

    My practice

---
+++ src/test/java/org/dsapractice/arrays/FirstMissingPositive.java
@@ -0,0 +1,44 @@
+package org.dsapractice.arrays;
+
+import java.util.Arrays;
+
+public class FirstMissingPositive {
+
+    public static void main(String[] args) {
+        int[] nums = {9,1,10};
+        System.out.println(firstMissingPOsitive(nums));
+    }
+
+    public static int firstMissingPOsitive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int expPos = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] <= nums.length) {
+                if (nums[i] != nums[expPos]) {
+                    swapArr(nums, i, expPos);
+                } else {
+                    i++;
+                }
+
+            } else {
+                i++;
+            }
+        }
+        int j = 0;
+        while (j < nums.length) {
+            if (nums[j] != j + 1) {
+                return j + 1;
+            }
+            j++;
+        }
+        return j + 1;
+    }
+
+    public static void swapArr(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+
+}
+
