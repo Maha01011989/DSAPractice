3445c9e1ea3d00d1b7942de9e6e4113ee3f714aa
Mahalakshmi <lekshmi0101@gmail.com>
2024-09-03 11:33:35 +0530

    My practice

--- src/test/java/org/dsapractice/sorting/MergeSortedArray.java
+++ src/test/java/org/dsapractice/sorting/MergeSortedArray.java
@@ -10,7 +10,7 @@ public class MergeSortedArray {
 
         int[] nums1 = {1, 2, 3, 0, 0, 0};
         int m = 3;
-        int[] nums2 = {2,5,6};
+        int[] nums2 = {4,5,6};
         int n = 3;
         merge(nums1, m, nums2, n);
 
