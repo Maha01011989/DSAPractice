3edeb0f701af4a4e6cf7daecdf7485b54c7d2da2
Mahalakshmi <lekshmi0101@gmail.com>
2024-09-05 11:20:25 +0530

    My practice

---
+++ src/test/java/org/dsapractice/searching/Ceiling.java
@@ -0,0 +1,30 @@
+package org.dsapractice.searching;
+
+public class Ceiling {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+
+        System.out.println(ceilingNumber(arr, 15));
+    }
+
+    static int ceilingNumber(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return arr[mid];
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            }
+        }
+
+        return arr[start];
+    }
+}
