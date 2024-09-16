5dfcb791a18ea6803e8f0e9f9e1d00b8ee6280c3
Mahalakshmi <lekshmi0101@gmail.com>
2024-09-16 13:12:03 +0530

    My practice

---
+++ src/test/java/org/dsapractice/arrays/AddNumbers.java
@@ -0,0 +1,23 @@
+package org.dsapractice.arrays;
+
+public class AddNumbers {
+
+    public static void main(String[] args) {
+        System.out.println(addDigits(38));
+    }
+
+    public static int addDigits(int num) {
+
+        int sum = 0;
+        do {
+            while (num > 0) {
+                sum += num % 10;
+                num = num / 10;
+            }
+            num = sum;
+            sum = 0;
+        }
+        while (num >= 10);
+        return num;
+    }
+}
