d375e81d700869e3cfa0fe80564acca2ab622e86
Mahalakshmi <lekshmi0101@gmail.com>
2024-09-17 12:46:13 +0530

    My practice

--- src/test/java/org/dsapractice/arrays/PLusOne.java
+++ src/test/java/org/dsapractice/arrays/PLusOne.java
@@ -6,20 +6,29 @@ public class PLusOne {
 
     public static void main(String[] args) {
 
-        int[] nums = {1, 2, 3};
-        int sum = 0;
+        int[] nums = {9, 9, 9, 2};
 
-        for (int i = 0; i < nums.length; i++) {
-            sum += (int) (nums[i] * Math.pow(10, nums.length - 1 - i));
+        System.out.println(Arrays.toString(plusOne(nums)));
+
+    }
+
+    public static int[] plusOne(int[] digits) {
+        int newArr[] = new int[digits.length + 1];
+
+        int i = digits.length - 1;
+        digits[i] += 1;
+
+
+        while (i >= 1 && digits[i] == 10) {
+            digits[i] = 0;
+            digits[i - 1] += 1;
+            i--;
         }
 
-        sum += 1;
-        System.out.println(sum + " ");
-        String s = sum + "";
-        int[] arr = new int[s.length()];
-
-        for (int i = 0; i < s.length(); i++) {
-            arr[i] = s.charAt(i) - '0';
+        if (digits[0] == 10) {
+            newArr[0] = 1;
+            return newArr;
         }
+        return digits;
     }
 }
