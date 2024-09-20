5dedc2bb459b7de6db469c732e216456ce3e9078
Mahalakshmi <lekshmi0101@gmail.com>
2024-09-20 19:20:39 +0530

    My practice

--- src/test/java/org/dsapractice/arrays/MaxProfitStock.java
+++ src/test/java/org/dsapractice/arrays/MaxProfitStock.java
@@ -5,16 +5,11 @@ public class MaxProfitStock {
 
     public static void main(String[] args) {
         int maxProfit = 0;
-        int[] prices = {2,4,1};
-        int profit = 0;
+        int[] prices = {7,6,4,3,1};
         for (int i = 0; i < prices.length; i++) {
             for (int j = i + 1; j < prices.length; j++) {
-                if (prices[i] > prices[j]) {
-                    continue;
-                } else {
-                    profit = prices[j] - prices[i];
-                    if (profit > maxProfit)
-                        maxProfit = profit;
+                if (prices[j] - prices[i] > maxProfit) {
+                    maxProfit = prices[j] - prices[i];
                 }
             }
         }
